228fca057c36c955c19f21804c5564bbd941555d
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2017-07-10 10:59:41 -0300

    Revert "Revert "adding rbenv+ruby-build combo on AUR""
    This reverts commit 88ef5844e463af2f3fd505d93f0193c798ec66b7.

---
+++ AUR/ruby-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p tmp
+
+BASEPATH=`pwd`
+FILEPATH=./tmp/google-chrome.tar.gz
+ENDPATH=./tmp/chrome
+
+curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/ruby-build.tar.gz \
+	--output $FILEPATH
+
+mkdir -p $ENDPATH
+
+tar -xvf $FILEPATH -C $ENDPATH --strip-components 1
+
+cd $ENDPATH && makepkg -si --noconfirm && cd $BASEPATH
+
+rm -rf ./tmp
