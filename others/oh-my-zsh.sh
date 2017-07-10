57912e56051b2c9acfb09d095d9de0da72a0443e
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2017-07-10 07:03:34 -0300

    adding a linking .zshrc on other

--- others/oh-my-zsh.sh
+++ others/oh-my-zsh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BASEPATH=`pwd`
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 rm -rf $HOME/.oh-my-zsh
@@ -7,3 +9,11 @@ rm -rf $HOME/.oh-my-zsh
 #changing to my oh-my-zsh fork
 git clone https://github.com/BAlmeidaS/oh-my-zsh $HOME/.oh-my-zsh
 cd $HOME/.oh-my-zsh && git fetch --all && git checkout my-own
+
+if [ -e $HOME/.zshrc ]
+then
+	echo "ERROR CREATE LINK ZSHRC - .zshrc already exists"
+	exit
+fi
+
+ln -s $BASEPATH/.zshrc $HOME/
