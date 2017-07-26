9637bcd44d9cde3b3b13521762e6bb6c2f176638
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2017-07-26 14:17:56 -0300

    adding haroopad on aur projects

---
+++ AUR/haroopad.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./ncurses.sh
+
+mkdir -p tmp
+
+BASEPATH=`pwd`
+FILEPATH=./tmp/pkg-compressed.tar.gz
+ENDPATH=./tmp/pkg
+
+curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/haroopad.tar.gz \
+	--output $FILEPATH
+
+mkdir -p $ENDPATH
+
+tar -xvf $FILEPATH -C $ENDPATH --strip-components 1
+
+cd $ENDPATH && makepkg -si --noconfirm && cd $BASEPATH
+
+rm -rf ./tmp
