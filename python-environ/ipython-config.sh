a119041b1686efeb72e784bd438e0fb064721219
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-11 00:18:47 -0300

    adding an error on script if ipython folder already exists

--- python-environ/ipython-config.sh
+++ python-environ/ipython-config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -e $HOME/.ipython ]
+then
+	echo "ERROR CREATE LINK IPYTHON - .ipython already exists on your home"
+	exit
+fi
+
 ln -s $PWD/dot-ipython $HOME
 mv $HOME/dot-ipython $HOME/.ipython
