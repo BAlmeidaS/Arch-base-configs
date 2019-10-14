bbe71e9b07b38e16213ecbf7a52355264c210363
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-10-14 20:41:31 -0300

    fix vundle script

--- others/vundle.sh
+++ others/vundle.sh
@@ -5,7 +5,6 @@ BASEPATH=$(cd $(dirname $BASH_SOURCE) && pwd )
 rm -rf $HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-vim +PluginInstall +qall
-
 ln -sf $BASEPATH/.vimrc $HOME/
 
+vim +PluginInstall +qall
