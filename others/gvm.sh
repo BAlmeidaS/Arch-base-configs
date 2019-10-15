d1967dd62625bfcd1e91392cc9eb8ef846dda345
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-10-14 21:19:56 -0300

    adding go 1.13 in gvm script

--- others/gvm.sh
+++ others/gvm.sh
@@ -4,3 +4,8 @@ sudo apt install bison -y
 
 zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 gvm install go1.4 -B
+
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.13
+gvm use go1.13
