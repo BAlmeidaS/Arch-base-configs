8ad810588aba50fedfd46d0aaa953f94fd085600
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-10-14 21:37:41 -0300

    minor changes in base pip script

--- others/base-pips.sh
+++ others/base-pips.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pip install --upgrade --user ipython
+sudo ln -sf /usr/bin/python3 /usr/local/bin/python3
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+python3 get-pip.py --user
+
+rm get-pip.py
+
+pip install virtualenvwrapper --user
