db17365618062f45c506ff06518123f54ec6fb99
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-10-17 00:21:07 -0300

    more packages on mujoco debian script

--- debian/mujoco.sh
+++ debian/mujoco.sh
@@ -6,3 +6,7 @@ sudo apt update
 sudo apt install libosmesa6-dev -y
 sudo apt install patchelf -y
 
+sudo apt-get install -y python-dev pkg-config
+
+sudo apt-get install -y libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libswscale-dev libavresample-dev libavfilter-dev
+
