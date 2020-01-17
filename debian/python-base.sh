69366dada0ab9eac2634d96677f265a6dc524382
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2020-01-17 14:32:34 -0300

    adding python base script in debian folder

---
+++ debian/python-base.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
