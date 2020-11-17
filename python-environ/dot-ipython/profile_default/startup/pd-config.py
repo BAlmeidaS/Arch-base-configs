f4a7ac91acebfd927b4015d850a2804adf8eb10f
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2020-11-16 23:57:32 -0300

    addind on profile def startup a validation if pandas could be imported

--- python-environ/dot-ipython/profile_default/startup/pd-config.py
+++ python-environ/dot-ipython/profile_default/startup/pd-config.py
@@ -1,5 +1,8 @@
-import pandas as pd
+try:
+    import pandas as pd
 
-pd.set_option('display.max_rows', 100)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+    pd.set_option('display.max_rows', 100)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+except ModuleNotFoundError:
+    pass
